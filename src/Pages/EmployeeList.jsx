adf443b3c3495b0c251f9cd64642721de53dec7c
Belarif <b.ocine@live.fr>
2024-01-07 17:43:39 +0100

    nhr1: adding sorters to table columns

--- src/Pages/EmployeeList.jsx
+++ src/Pages/EmployeeList.jsx
@@ -5,56 +5,120 @@ import { Table } from "antd";
 import { selectEmployeesData } from "../selectors/selectors";
 
 const EmployeeList = () => {
+  const employeesData = useSelector(selectEmployeesData);
+  const employeesList = employeesData["employees"];
+
   const columns = [
     {
       title: "First Name",
       dataIndex: "firstName",
+      sorter: (a, b) => {
+        if (a.firstName < b.firstName) {
+          return -1;
+        }
+        if (a.firstName > b.firstName) {
+          return 1;
+        }
+        return 0;
+      },
     },
     {
       title: "Last Name",
       dataIndex: "lastName",
+      sorter: (a, b) => {
+        if (a.lastName < b.lastName) {
+          return -1;
+        }
+        if (a.lastName > b.lastName) {
+          return 1;
+        }
+        return 0;
+      },
     },
     {
       title: "Start Date",
       dataIndex: "startDate",
-      sorter: {
-        compare: (a, b) => a.startDate - b.startDate,
+      sorter: (a, b) => {
+        if (a.startDate < b.startDate) {
+          return -1;
+        }
+        if (a.startDate > b.startDate) {
+          return 1;
+        }
+        return 0;
       },
     },
     {
       title: "Department",
       dataIndex: "department",
+      sorter: (a, b) => {
+        if (a.department < b.department) {
+          return -1;
+        }
+        if (a.department > b.department) {
+          return 1;
+        }
+        return 0;
+      },
     },
     {
       title: "Date of Birth",
       dataIndex: "dateOfBirth",
-      sorter: {
-        compare: (a, b) => a.dateOfBirth - b.dateOfBirth,
+      sorter: (a, b) => {
+        if (a.dateOfBirth < b.dateOfBirth) {
+          return -1;
+        }
+        if (a.dateOfBirth > b.dateOfBirth) {
+          return 1;
+        }
+        return 0;
       },
     },
     {
       title: "Street",
       dataIndex: "street",
+      sorter: (a, b) => {
+        if (a.street < b.street) {
+          return -1;
+        }
+        if (a.street > b.street) {
+          return 1;
+        }
+        return 0;
+      },
     },
     {
       title: "City",
       dataIndex: "city",
+      sorter: (a, b) => {
+        if (a.city < b.city) {
+          return -1;
+        }
+        if (a.city > b.city) {
+          return 1;
+        }
+        return 0;
+      },
     },
     {
       title: "State",
       dataIndex: "state",
+      sorter: (a, b) => {
+        if (a.state < b.state) {
+          return -1;
+        }
+        if (a.state > b.state) {
+          return 1;
+        }
+        return 0;
+      },
     },
     {
       title: "Zip Code",
       dataIndex: "zipCode",
-      sorter: {
-        compare: (a, b) => a.zipCode - b.zipCode,
-      },
+      sorter: (a, b) => a.zipCode - b.zipCode,
     },
   ];
-
-  const employeesData = useSelector(selectEmployeesData);
-  const employeesList = employeesData["employees"];
 
   return (
     <React.Fragment>
